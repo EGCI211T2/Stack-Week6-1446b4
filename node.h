5067fca0d15726ebb88b5c26f2b09bff771e73a8
AjMing <62439174+AjMing@users.noreply.github.com>
2026-03-04 00:00:35 +0000

    Create template

--- node.h
+++ node.h
@@ -5,15 +5,49 @@
 //  Created by Mingmanas Sivaraksa
 //
 
-#ifndef linkedlist_h
-#define linkedlist_h
-struct node
-{
-    int data;
-    struct node *nextPtr;
+#ifndef node_h
+#define node_h
+class NODE{
+     int data;
+    NODE *nextPtr;
+public:
+    NODE(int);
+    ~NODE();
+    void set_next(NODE*);
+    NODE* get_next();
+    int get_value();
 };
+typedef NODE* NodePtr;
 
-typedef struct node Node;
-typedef struct node* NodePtr;
+
+NODE::NODE(int x){
+    data=x;
+    nextPtr=NULL;
+    //pPtr =NULL; not using doubly
+}
+NODE* NODE::get_next(){
+    return nextPtr;
+
+}
+
+int NODE::get_value(){
+    return data;
+
+}
+
+
+void NODE::set_next(NODE *t){
+     nextPtr=t;
+
+}
+NODE::~NODE(){
+     cout<<"deleting "<<data<<endl;
+
+}
+
+
+
+
+
 
 #endif
--- stack.h
+++ stack.h
@@ -2,13 +2,45 @@
 #ifndef stack_h
 #define stack_h
 #include "node.h"
-typedef struct {
+class Stack {
 	NodePtr top;
 	int size;
-}Stack;
+public:
+    void push(int);
+    int pop();
+    Stack();
+    ~Stack();
+};
 
-typedef Stack * StackPtr;
-void push(StackPtr s, char value);
-char pop(StackPtr s);
-void pop_all(StackPtr s);
+
+void Stack::push(int x){
+  NodePtr new_node=new NODE(x);
+  if(new_node){
+            // Left missing for exercises…
+   }
+ 
+         // Left missing for exercises…
+    
+}
+
+int Stack::pop(){
+        NodePtr t=top;
+        int value;
+        value=t->get_value();
+    // Left missing part for exercises
+        delete t;
+        return value;
+	//be careful of the empty stack!!!
+    }
+
+Stack::Stack(){
+    //initialize stack
+    
+}
+Stack::~Stack(){
+    //delete all remaning stack (i.e. pop all) 
+    
+}
+
+
 #endif
